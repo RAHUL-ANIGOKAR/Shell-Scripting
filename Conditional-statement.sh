1d604ada53d653da87eb095b0fda43c125071e38
root <root@ip-172-31-3-67.ap-south-1.compute.internal>
2025-07-14 12:12:03 +0000

    Conditional Statements

--- Conditional-statement.sh
+++ Conditional-statement.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 read -p "Enter the number :" num
-
+read -p "Enter the Country :" Con
 if [[ $num == 10 ]];
 then
 	echo "Its Messi"
+elif [[ $Con == Argentina ]]
+then
+	echo "Yes its Messi"
 else
 	echo "Its not Messi"
 fi
